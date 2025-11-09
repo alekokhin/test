0fc2470c8a77a2fecfc1ac961ec14230a213f3d4
alekokhin <103178464+alekokhin@users.noreply.github.com>
2025-11-09 13:13:07 +0400

    upload project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import TestComponent from "./components/main";
 
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  useEffect(() => {
+    console.log("hello world");
+  }, [num1]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestComponent />
+      <button onClick={() => setNum1((prev) => prev + 1)}>+num1</button>
+      <button onClick={() => setNum2((prev) => prev + 1)}>+num2</button>
     </div>
   );
 }
---
+++ src/components/main/index.jsx
@@ -0,0 +1,13 @@
+const TestComponent = () => {
+  return (
+    <div
+      style={{
+        width: "300px",
+        height: "300px",
+        backgroundColor: "lightblue",
+      }}
+    ></div>
+  );
+};
+
+export default TestComponent;
